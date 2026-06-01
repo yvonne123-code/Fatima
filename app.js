360ac45a092863e7e19fd8977d8742f447c59a22
Isa Forsberg <isautanede@gmail.com>
2026-06-01 11:07:56 +0200

    added functions

--- app.js
+++ app.js
@@ -2,3 +2,18 @@ console.log("hej")
 
 TODO: lägg till funktionerendless-loop();
 console.log("hejdå")
+
+function endless-loop() {
+    while (true) {
+        console.log("loopar")
+    }
+}
+
+endless-loop()
+
+function greet(name) {
+    console.log("hej " + name)
+}
+
+greet("Alice")
+greet("Bob")
